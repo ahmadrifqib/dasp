a25dbccaf5de4850101b2fc7efc7b56609235dd6
ahmadrifqib <ahmad.rifqi@outlook.co.id>
2022-10-31 13:40:13 +0700

    initial push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '../styles/globals.css'
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
+import { HeaderMenu } from "../components/HeaderMenu";
+import type { HeaderSearchProps } from "../components/HeaderMenu";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	// links: { link: string; label: string; links?: { link: string; label: string }[] }[];
+	const links: HeaderSearchProps = {
+		links: [{ link: "/", label: "Beranda" }],
+	};
+	return (
+		<MantineProvider>
+			<Head>
+				<title>PT DWI AGUNG SENTOSA PRATAMA</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<HeaderMenu links={links.links} />
+			<Component {...pageProps} />
+		</MantineProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
